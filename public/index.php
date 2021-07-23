dc5438d433efab6ce148ccaa68568a7ceba93c9d
khadija <khadijalahmar@gmail.com>
2021-07-23 21:29:12 +0100

    add_structeur_mvc

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+// a chaque fois initialisation index
+// louad constant
+  require_once '../app/bootstrap.php';
+//  entre index travailler  construct object    de cor 
+  // Init Core Libririe
+  // CONSTRUCTION D'OBJET DONC _CONSTRUCT WILL WORK
+  $init = new Core;
+  // htaccess public redirect crée url 

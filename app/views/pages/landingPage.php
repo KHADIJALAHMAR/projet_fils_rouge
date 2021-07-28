95ebcd01972719c0bf2ca908c5295bd8a5a18a94
khadija <khadijalahmar@gmail.com>
2021-07-28 12:01:00 +0100

    add_section_header

--- app/views/pages/landingPage.php
+++ app/views/pages/landingPage.php
@@ -1,6 +1,18 @@
 <?php include_once APPROOT . '/views/inc/header.php'; ?>
 <?php include_once APPROOT . '/views/inc/navbar.php'; ?>
 
- 
+    <main>
+
+        <div class="position-relative bg-second hero-header">
+            <div>
+                
+            </div>
+            <div class="wave position-absolute w-100">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                <path fill="#E8E4E6" fill-opacity="1" d="M0,128L720,320L1440,32L1440,320L720,320L0,320Z"></path>
+            </svg>
+            </div>
+        </div>
+    </main>
 
 <?php include_once APPROOT . '/views/inc/footer.php'; ?>

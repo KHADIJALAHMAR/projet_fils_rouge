b92546c8ba5538aedbaae2dff0ec201b5350ced6
khadija <khadijalahmar@gmail.com>
2021-07-30 23:14:56 +0100

    add_footer_landing_page

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -2,4 +2,64 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 </body>
+<footer class="bg-success text-white text-center text-lg-start">
+  <!-- Grid container -->
+  <div class="container p-4">
+    <!--Grid row-->
+    <div class="row">
+      <!--Grid column-->
+      <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+        <h5 class="text-uppercase">YOPHARMA</h5>
+      </div>
+      <!--Grid column-->
+
+      <!--Grid column-->
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+        <h5 class="text-uppercase">COMPANY</h5>
+
+        <ul class="list-unstyled mb-0">
+          <li>
+            <About href="#!" class="text-white">About us</a>
+          </li>
+          <li>
+            <a href="#!" class="text-white">Contact us</a>
+          </li>
+          <li>
+            <a href="#!" class="text-white">what's new</a>
+          </li>
+        </ul>
+      </div>
+      <!--Grid column-->
+
+      <!--Grid column-->
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+        <h5 class="text-uppercase mb-0">SOCIAL MEDIA</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!" class="text-white">facebook</a>
+          </li>
+          <li>
+            <a href="#!" class="text-white">twitter</a>
+          </li>
+          <li>
+            <a href="#!" class="text-white">instagram</a>
+          </li>
+          <li>
+            <a href="#!" class="text-white">linked in</a>
+          </li>
+        </ul>
+      </div>
+      <!--Grid column-->
+    </div>
+    <!--Grid row-->
+  </div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    <a class="text-white" href="https://mdbootstrap.com/">Copyright © 2021 YOPHARMA All rights reserved.</a>
+  </div>
+  <!-- Copyright -->
+</footer>
 </html>

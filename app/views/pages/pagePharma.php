36ed7f27a5bcbf71423c9cebc0509f39acfbcae9
khadija <khadijalahmar@gmail.com>
2021-08-03 14:50:18 +0100

    add_style_arrow

--- app/views/pages/pagePharma.php
+++ app/views/pages/pagePharma.php
@@ -1,4 +1,12 @@
 <?php include_once APPROOT . '/views/inc/headerp.php'; ?>
+<div class="parent-pharma row content-p  ">
+<div class="child-1 ">
+    <p style="color:#E8E4E6">Gard pharmacies</p>
+</div>
+    <div class="child-2 ">
+
+    </div>
+</div>
 
 
 

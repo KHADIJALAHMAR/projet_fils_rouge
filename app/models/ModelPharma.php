bc7b68fa9199671bd3f8c7b413954b96274fc2a1
khadija <khadijalahmar@gmail.com>
2021-08-03 09:38:30 +0100

    create_method_addpharma

--- app/models/ModelPharma.php
+++ app/models/ModelPharma.php
@@ -11,4 +11,21 @@ public function show(){
     $results = $this->db->single();
     return $results;
 }
+public function add($data){
+    $this->db->query("INSERT INTO `pharmacy` ( `localisation`, `image`, `phone`) VALUES ( :localisation,:image,:phone)");
+    $this->db->bind(':localisation', $data['localisation']);
+    $this->db->bind(':image', $data['image']);
+    $this->db->bind(':phone', $data['phone']);
+    $exucute_data=$this->db->execute();
+    return $exucute_data;
+    if($exucute_data)
+    {
+        echo "insert  exist";
+    }else{
+        echo"insert does not exist";
+    }
+
+
+
+}
 }

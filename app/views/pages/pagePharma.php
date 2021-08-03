b2804fa5bb92fd3f8d3858139a1ceed94c96c45d
khadija <khadijalahmar@gmail.com>
2021-08-03 16:16:05 +0100

    add_imgPharma_gard

--- app/views/pages/pagePharma.php
+++ app/views/pages/pagePharma.php
@@ -1,4 +1,5 @@
 <?php include_once APPROOT . '/views/inc/headerp.php'; ?>
+<!-- flèche pharmacie de gard -->
 <div class="parent-pharma row content-p  ">
 <div class="child-1 ">
     <p style="color:#E8E4E6">Gard pharmacies</p>
@@ -6,6 +7,19 @@
     <div class="child-2 ">
 
     </div>
+</div>
+<!-- pharma gard -->
+<div class="content-p">
+<div class="card content " style="width: 18rem;">
+    <img src="" class="card-img-top" alt="img">
+    <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <a href="#" class="card-link">Localisation</a>
+    <p class="card-text">Numéro de téléphone</p>
+
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
 </div>
 
 

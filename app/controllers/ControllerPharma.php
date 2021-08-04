1b66f4a77bc286afe37081d14eedaba3a4f1a88d
khadija <khadijalahmar@gmail.com>
2021-08-04 11:10:33 +0100

    create_methode_insertController

--- app/controllers/ControllerPharma.php
+++ app/controllers/ControllerPharma.php
@@ -7,7 +7,33 @@ class ControllerPharma extends Controller{
         $result = $this->model->show();
         $this->view('pages/pagePharma',$result);
     }
-
-
+    public function insert()
+    {
+    if (isset($_POST["submit"])) {
+     $image=$_FILES['image']['tmp_name'];
+      $data=[
+     'name_pharma' =>$_POST['name_pharma'],
+     'localisation' => $_POST['localisation'],
+     'phone'=>$_POST['phone'],
+     'image' => $_FILES['image']['name'],
+     
+      ];
+      if($this->uploadPhoto($image)===true){
+       if( $this->model->addPost($data) ){
+         
+         header('location:'.URLROOT .'/ControllerPharma/index');
+         
+      } else{
+         die('Something went wrong');
+      }
+   }else{
+      die('Something went wrong');
+   }
+     
+    }
     
+      $this->view('pages/insert');
+    }
+
+
 }

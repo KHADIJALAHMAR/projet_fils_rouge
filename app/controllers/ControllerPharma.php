d0e68675cd487a7b78da1a3b17aacbce36ada8d4
khadija <khadijalahmar@gmail.com>
2021-08-04 11:02:01 +0100

    add_model_in_FilsController

--- app/controllers/ControllerPharma.php
+++ app/controllers/ControllerPharma.php
@@ -1,11 +1,12 @@
 <?php
 class ControllerPharma extends Controller{
     public function __construct(){
-       
+        $this->model= $this->model('ModelPharma');
     }
     public function index(){
         $this->view('pages/pagePharma');
     }
-    
+
+
     
 }

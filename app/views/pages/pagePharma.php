235f11f9db75b3a74687e02f0542c0a940fda740
khadija <khadijalahmar@gmail.com>
2021-08-04 09:38:45 +0100

    add_card_pharmacie_Front-End

--- app/views/pages/pagePharma.php
+++ app/views/pages/pagePharma.php
@@ -23,7 +23,20 @@
 </div>
 <!-- button -->
 <button>Ajouter</button>
+<!-- crd tous pharma -->
+<div class="content-p">
+<div class="card content " style="width: 18rem;">
+    <img src="<?php echo URLROOT; ?>/assets/img/pharma1.jpg" class="card-img-top" alt="img">
+    <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <a href="#" class="card-link">Localisation</a>
+    <p class="card-text">Numéro de téléphone</p>
 
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</div>
+<!-- maps -->
 
 
 

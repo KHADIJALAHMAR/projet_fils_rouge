c9fa46cb579d44a97d88e392466c6171a96594e0
khadija <khadijalahmar@gmail.com>
2021-08-05 14:42:18 +0100

    finish_validation_signup_Affiche_message-err_View

--- app/controllers/ControllerCompte.php
+++ app/controllers/ControllerCompte.php
@@ -1,93 +1,219 @@
 <?php
-  class ControllerCompte extends Controller {
-    public function __construct(){
-        $this->model= $this->model('ModelCompte');
-        $this->Session = new Session;
+class ControllerCompte extends Controller
+{
+  public function __construct()
+  {
+    $this->model = $this->model('ModelCompte');
+    $this->Session = new Session;
+  }
 
-    }
+  public function index()
+  {
+    $this->view('pages/signup');
+  }
+  public function insert()
+  {
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+      $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+      // Init data
+      $data = [
+        'fname'                => trim($_POST['name']),
+        'lname'                => trim($_POST['lname']),
+        'email'                => trim($_POST['email']),
+        'pass_word'            => trim($_POST['password']),
+        'role'                 => trim($_POST['role']),
+        'name_err'             => '',
+        'lname_err'             => '',
+        'email_err'            => '',
+        'pass_word_err'         => '',
+        'role_err'             => '',
+      ];
+      // validation
+      if (empty($data['email'])) {
+        $data['email_err'] = 'Please enter email';
+      } else {
+        // Check email
+        if ($this->model->findUser($data['email'])) {
+          $data['email_err'] = 'Email already taken';
+        }
+      }
 
-    public function index(){
-        $this->view('pages/signup');
-    }
-    public function insert()
-    {
-    if (isset($_POST["submit"])) {
-        $data =[
-            'fname' =>$_POST['name'],
-            'lname' => $_POST['lname'],
-            'email' => $_POST['email'],
-            'pass_word'=>$_POST['password'],
-            'role'=>$_POST['role'],
+
+      // Validate Name
+      if (empty($data['fname'])) {
+        $data['name_err'] = 'Please enter name';
+      }
+
+       // Validate lName
+       if (empty($data['lname'])) {
+        $data['lname_err'] = 'Please enter lname';
+      }
+
+       // Validate Name
+       if (empty($data['role'])) {
+        $data['name_err'] = 'Please enter role';
+      }
+
+      // Validate Password
+      if (empty($data['pass_word'])) {
+        $data['password_err'] = 'Please enter password';
+      } elseif (strlen($data['pass_word']) < 6) {
+        $data['password_err'] = "Password must be at least 6 characters";
+      }
+
+      // check input is not empty
+      if (empty($data['email_err']) && empty($data['name_err']) && empty($data['password_err'])) {
+        // Hash Password
+        // $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
+        // Register User
+        if ($this->model->signup($data)) {
+          // Notify before redirect
+          $this->view('pages/login');
+        } else {
+          die('Something went wrong');
+        }
+
+        $this->view('pages/signup', $data);
+        
+      } else {
+    
+        $this->view('pages/signup', $data);
+      }
+       // Init data
+      $data = [
+          'name'                 => '',
+          'email'                => '',
+          'pass_word'             => '',
+          'confirm_password'     => '',
+          'name_err'             => '',
+          'email_err'            => '',
+          'pass_word_err'         => '',
+          'role_err'             => '',
         ];
-        // validation
-        if (empty($data['email'])) {
-          echo "please enter your email";
-      }
-       //validation password
-       if (empty($data['pass_word'])) {
-        echo "please enter your pass_word";
-    }
-    elseif (strlen($data['pass_word']) < 5) {
-        echo "password must be at least 5 characters";
-    }
-    if ($this->model->signup($data)) {
-
-      $this->view('pages/login');
-     }
-        // check if email already exists
-        if($this->model->findUser('email')){
-          echo "email already taken";
-        }
-        else{
-          $this->view('pages/signup');
-        }
-
-      }
-}
-  public function select()
-    {
-
-        $this->Session->startSession();
-
-        // if (isset($_SESSION['email'])) {
-
-        //     header('location:' . URLROOT . '/ProfilController/pageProfil');
-        // }
-
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
-            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
-
-            $data = [
-                'email' => $_POST['email'],
-                'pass_word' => $_POST['password']
-            ];
-            //validation email
-            if (empty($data['email'])) {
-                echo "please enter your email";
-            }
-            //validation password
-            if (empty($data['pass_word'])) {
-                echo "please enter your password";
-            }
-            //check for email et password
-            if ($user =$this->model->login($data['email'],$data['pass_word'])) {
-
-                $this->Session->setSession('id_user',$user->userId);
-                $this->Session->setSession('username',$user->username);
-
-                ////////
-                header('location:' . URLROOT . '/ControllerCompte/index');
-                ///////
-
-            }
-            else {
-                echo "please enter a valid email";
-            }
-
-        }
-        else {
-            $this->view('pages/Login');
-        }
+        // Load view
+        $this->view('pages/signup', $data);
     }
   }
+  // public function select()
+  //   {
+
+  //       $this->Session->startSession();
+
+  //       // if (isset($_SESSION['email'])) {
+
+  //       //     header('location:' . URLROOT . '/ProfilController/pageProfil');
+  //       // }
+
+  //       if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+  //           $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+  //           $data = [
+  //               'email' => $_POST['email'],
+  //               'pass_word' => $_POST['password']
+  //           ];
+  //           //validation email
+  //           if (empty($data['email'])) {
+  //               echo "please enter your email";
+  //           }
+  //           //validation password
+  //           if (empty($data['pass_word'])) {
+  //               echo "please enter your password";
+  //           }
+  //           //check for email et password
+  //           if ($user =$this->model->login($data['email'],$data['pass_word'])) {
+
+  //               $this->Session->setSession('id_user',$user->userId);
+  //               $this->Session->setSession('username',$user->username);
+
+  //               ////////
+  //               header('location:' . URLROOT . '/ControllerCompte/index');
+  //               ///////
+
+  //           }
+  //           else {
+  //               echo "please enter a valid email";
+  //           }
+
+  //       }
+  //       else {
+  //           $this->view('pages/Login');
+  //       }
+  //   }
+}
+  ################################################################"
+  
+  // public function register()
+  // {
+  //     // Check for POST
+  //     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  //         // Sanitize POST data
+  //         $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+  //         // Init data
+  //         $data = [
+  //             'name'                 => trim($_POST['name']),
+  //             'email'                => trim($_POST['email']),
+  //             'password'             => trim($_POST['password']),
+  //             'confirm_password'     => trim($_POST['confirm_password']),
+  //             'name_err'             => '',
+  //             'email_err'            => '',
+  //             'password_err'         => '',
+  //             'confirm_password_err' => '',
+  //         ];
+  //         // Validate Email
+  //         if (empty($data['email'])) {
+  //             $data['email_err'] = 'Please enter email';
+  //         } else {
+  //             // Check email
+  //             if ($this->userModel->findUserByEmail($data['email'])) {
+  //                 $data['email_err'] = 'Email already taken';
+  //             }
+  //         }
+  //         // Validate Name
+  //         if (empty($data['name'])) {
+  //             $data['name_err'] = 'Please enter name';
+  //         }
+  //         // Validate Password
+  //         if (empty($data['password'])) {
+  //             $data['password_err'] = 'Please enter password';
+  //         } elseif (strlen($data['password']) < 6) {
+  //             $data['password_err'] = "Password must be at least 6 characters";
+  //         }
+  //         // Validate Confirm Password
+  //         if (empty($data['confirm_password'])) {
+  //             $data['confirm_password_err'] = 'Please confirm password';
+  //         } else {
+  //             if ($data['password'] != $data['confirm_password']) {
+  //                 $data['confirm_password_err'] = 'Passwords do not match';
+  //             }
+  //         }
+  //         // Make sure errors are empty
+  //         if (empty($data['email_err']) && empty($data['name_err']) && empty($data['password_err']) && empty($data['confirm_password_err'])) {
+  //             // Hash Password
+  //             $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
+  //             // Register User
+  //             if ($this->userModel->register($data)) {
+  //                 // Notify before redirect
+  //                 redirect('users/login');
+  //             } else {
+  //                 die('Something went wrong');
+  //             }
+  //         } else {
+  //             // Load view with errors
+  //             $this->view('users/register', $data);
+  //         }
+  //     } else {
+  //         // Init data
+  //         $data = [
+  //             'name'                 => '',
+  //             'email'                => '',
+  //             'password'             => '',
+  //             'confirm_password'     => '',
+  //             'name_err'             => '',
+  //             'email_err'            => '',
+  //             'password_err'         => '',
+  //             'confirm_password_err' => '',
+  //         ];
+  //         // Load view
+  //         $this->view('users/register', $data);
+  //     }
--- app/views/pages/signup.php
+++ app/views/pages/signup.php
@@ -1,32 +1,41 @@
 <?php include_once APPROOT . '/views/inc/header.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/style.css ?>">
-    <title>login</title>
-</head>
-<body>
-    <div class="formcontainer">
-        <div class="cardform">
-        <form  class="signupform " action="<?php echo URLROOT; ?>/ControllerCompte/insert" method="POST">
-        <h1>Create account</h1>
-        <input   class="style-input" type="text" name="name" placeholder="name" >
-        <input  class="style-input" type="text" name="lname"  placeholder="lname">
-        <input  class="style-input" type="text" name="email"  placeholder="email">
-        <input  class="style-input" type="password" name="password"  placeholder="password">
-        <label  style="color:#E8E4E6" for="cars" name="role">Choose a car:</label>
-<select class="style-input" id="cars" name="role">
-  <option > pharmasict </option>
-  <option> Admin </option>
-</select>
 
-<input  class="btn-card" type="submit" name="submit"  />
-<p style="color:#E8E4E6">You already have an account? <span style="color:#42B130"><a>Login</a></span></p>
-        </form>
-        </div>
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/style.css ?>">
+  <title>login</title>
+</head>
+
+<body >
+  <div class="formcontainer">
+    <div class="cardform">
+      <form class="signupform " action="<?php echo URLROOT; ?>/ControllerCompte/insert" method="POST">
+        <h1 style="color:#E8E4E6">Create account</h1>
+        <input class="style-input" type="text" name="name" placeholder="name"
+        <?php echo (!empty($data['name_err'])) ? 'is-invalid' : '' ?> value="<?php echo $data['fname'] ?? '' ?>">
+        <span style="color:#E8E4E6"><?php echo $data['name_err'] ?? '' ?></span>
+        <input class="style-input" type="text" name="lname" placeholder="lname" <?php echo (!empty($data['lname_err'])) ? 'is-invalid' : '' ?> value="<?php echo $data['lname'] ?? '' ?>">
+        <span style="color:#E8E4E6"><?php echo $data['lname_err'] ?? '' ?></span>
+        <input class="style-input" type="text" name="email" placeholder="email" <?php echo (!empty($data['email_err'])) ? 'is-invalid' : '' ?> value="<?php echo $data['email'] ?? '' ?>">
+        <span style="color:#E8E4E6"><?php echo $data['email_err'] ?? '' ?></span>
+
+        <input class="style-input" type="text" name="password" placeholder="password" <?php echo (!empty($data['pass_word_err'])) ? 'is-invalid' : '' ?> value="<?php echo $data['pass_word'] ?? '' ?>">
+        <span class="danger" style="color:#E8E4E6"><?php echo $data['password_err'] ?? '' ?></span>
+        <label style="color:#E8E4E6" for="cars" name="role">Choose a car:</label>
+        <select class="style-input" id="cars" name="role" <?php echo (!empty($data['role_err'])) ? 'is-invalid' : '' ?> value="<?php echo $data['role'] ?? '' ?>">
+          <option> pharmasict </option>
+          <option> Admin </option>
+        </select>
+
+        <input class="btn-card" type="submit" name="submit" />
+        <p style="color:#E8E4E6">You already have an account? <span style="color:#42B130"><a>Login</a></span></p>
+      </form>
     </div>
+  </div>
 </body>
+
 </html>

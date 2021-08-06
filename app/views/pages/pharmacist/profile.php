a23ecc566ce103a9af47c071e642aa90247433ae
khadija <khadijalahmar@gmail.com>
2021-08-06 22:20:55 +0100

    add_part_About_ProfilPharma

--- app/views/pages/pharmacist/profile.php
+++ app/views/pages/pharmacist/profile.php
@@ -1,7 +1,6 @@
 <?php include_once APPROOT . '/views/inc/header.php'; ?>
 <?php include_once APPROOT . '/views/inc/navbar.php';?>
 
-    
 
 <div class="row py-5 px-4">
     <div class="col-md-5 mx-auto">
@@ -9,14 +8,14 @@
         <div class="bg-white shadow rounded overflow-hidden">
             <div class="px-4 pt-0 pb-4 cover">
                 <div class="media align-items-end profile-head">
-                    <div class="profile mr-3"><img src="https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80" alt="..." width="130" class="rounded mb-2 img-thumbnail"><a href="#" class="btn btn-outline-dark btn-sm btn-block">Edit profile</a></div>
+                    <div class="profile mr-3"><img src="https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80" alt="..." width="130" class="rounded mb-2 img-thumbnail"><a href="#" class="btn btn-outline-success btn-sm btn-block">Edit profile</a></div>
                     <div class="media-body mb-5 text-dark">
-                        <h4 class="mt-0 mb-0">Mark Williams</h4>
-                        <p class="small mb-4"> <i class="fas fa-map-marker-alt mr-2"></i>New York</p>
+                        <h4 class="mt-0 mb-0 "  style="color:#42B130">Mark Williams</h4>
+                        <p class="small mb-4"> <i class="fas fa-map-marker-alt mr-2"></i>Youssoufia</p>
                     </div>
                 </div>
             </div>
-            <div class="bg-light p-4 d-flex justify-content-end text-center">
+            <!-- <div class="bg-light p-4 d-flex justify-content-end text-center">
                 <ul class="list-inline mb-0">
                     <li class="list-inline-item">
                         <h5 class="font-weight-bold mb-0 d-block">215</h5><small class="text-muted"> <i class="fas fa-image mr-1"></i>Photos</small>
@@ -26,12 +25,12 @@
                     </li>
                     </li>
                 </ul>
-            </div>
+            </div> -->
             <div class="px-4 py-3">
                 <h5 class="mb-0">About</h5>
                 <div class="p-4 rounded shadow-sm bg-light">
-                    <p class="font-italic mb-0"></p>
-                    <p class="font-italic mb-0">email</p>
+                    <h3 class="font-italic mb-0"  style="color:#42B130">gard pharmacies</h3>
+                    <p class="font-italic mb-0">we help you know the available gard pharmaciesevery day.</p>
                     <p class="font-italic mb-0"></p>
                 </div>
             </div>
